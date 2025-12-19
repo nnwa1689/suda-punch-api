60194376b50a1bcfb64fd52c28a67dd5d51b85ae
TedWu <nnwa1689@gmail.com>
2025-12-20 00:07:27 +0800

    加入 swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './common/response.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ResponseInterceptor());
+  const config = new DocumentBuilder()
+  .setTitle('Suda 速打打卡系統 API')
+  .setVersion('1.0')
+  .addBearerAuth() // 讓文件支援 JWT 測試
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
