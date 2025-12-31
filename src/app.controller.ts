cd89da0ad47096ecbf98d26a6fa9598b4c0cd527
TedWu <nnwa1689@gmail.com>
2026-01-01 00:26:18 +0800

    1. 調整api版本取得功能

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getVersion(): { data: string, message?: string } {
+    return { data: this.appService.getVersion(), message: 'suda_punch_api' };
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Suda Punch API v1.0.';
+  getVersion(): string {
+    return 'v1.0.1';
   }
 }
